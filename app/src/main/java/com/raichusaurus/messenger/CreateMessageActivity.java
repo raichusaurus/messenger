eef177e98bf5471172e1d1b0348ab25ccd674ee3
felixerus <felixerus@gmail.com>
2018-01-30 10:47:56 -0800

    Stub onSendMessage() to prevent app crash when button is clicked.

--- app/src/main/java/com/raichusaurus/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/raichusaurus/messenger/CreateMessageActivity.java
@@ -2,6 +2,7 @@ package com.raichusaurus.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class CreateMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    // call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
     }
 }
